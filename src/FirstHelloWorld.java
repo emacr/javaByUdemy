5959b98d544613b95903d3a187466c30f89b3760
emacr <manuelcarranzaw@gmail.com>
2025-04-16 10:03:00 -0600

    prueba

--- src/FirstHelloWorld.java
+++ src/FirstHelloWorld.java
@@ -2,5 +2,7 @@ public class FirstHelloWorld {
     public static void main(String[] args) {
         //print hello world
         System.out.println("Hello world");
+        String name = "emma";
+        System.out.println("mi nombre es: "+name);
     }
 }
